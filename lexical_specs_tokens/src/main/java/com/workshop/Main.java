cedc8b3dc43b9aa8ec8b70bdae72a1c782a46ab8
Sergio Majé <smajefranco@gmail.com>
2025-10-03 13:03:56 -0500

    🔥 Remove lexical_specs_tokens project

--- lexical_specs_tokens/src/main/java/com/workshop/Main.java
+++
@@ -1,7 +0,0 @@
-package com.workshop;
-
-public class Main {
-    public static void main(String[] args) {
-        LexerRunner.run("int x := 123;\nif x == 10 then print(x);\n// a comment\nx := x + 1.5;");
-    }
-}
