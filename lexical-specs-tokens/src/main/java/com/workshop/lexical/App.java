4ae865acfe765a60dee8d2206cbfd3529ede2f06
Sergio Majé <smajefranco@gmail.com>
2025-10-03 19:02:57 -0500

    🚧 Implemente Lexer structure
    Lexer structure: Token and TokenConstants (type)

--- lexical-specs-tokens/src/main/java/com/workshop/lexical/App.java
+++ lexical-specs-tokens/src/main/java/com/workshop/lexical/App.java
@@ -1,10 +1,29 @@
 package com.workshop.lexical;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App.run("int x = 10;\nif (x > 0) {\n    print(x);\n} else {\n    // Do nothing\n}");
+    }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    public static void run(String input) {
+        try (Reader reader = new StringReader(input)) {
+            GeneratedLexer lexer = new GeneratedLexer(reader);
+            Token token;
+            do {
+                token = lexer.yylex();
+                System.out.println(token);
+            } while (token.type() != TokenConstants.EOF);
+            System.out.println("Lexing completed.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ lexical-specs-tokens/src/main/java/com/workshop/lexical/TokenConstants.java
@@ -0,0 +1,19 @@
+package com.workshop.lexical;
+
+public enum TokenConstants {
+    EOF,
+    IDENTIFIER,
+    RESERVED,
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    ASSIGN,
+    EQUALS,
+    LPAREN,
+    RPAREN,
+    SEMICOLON,
+    COMMENT,
+    ERROR
+}
